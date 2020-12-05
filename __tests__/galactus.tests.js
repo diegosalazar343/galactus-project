c086bdbad0632ee4827332d0e8219458ea268b8f
diegosalazar343 <diegosalazar343@gmail.com>
2020-12-05 10:37:54 -0800

    how many years past mars life expectancy will pass

--- __tests__/galactus.tests.js
+++ __tests__/galactus.tests.js
@@ -61,6 +61,6 @@ test('Should create a GalactusCalculator object, using user input', () => {
 
   test('Should calculate marsPassed using the average life and users age', () => {
     const answer = new GalactusCalculator(55, 50);
-    expect(answer.marsPassed()).toEqual(8);
+    expect(answer.marsPassed()).toEqual(3);
   })
 });
